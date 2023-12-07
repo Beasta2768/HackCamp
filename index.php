4129290e19b6aae7adbea45614497583f2eb0d0b
AVilhelmsson <vilhelmssonalex76@gmail.com>
2023-12-07 15:49:28 +0000

    newest newest code

--- index.php
+++ index.php
@@ -2,5 +2,10 @@
 $view = new stdClass();
 $view->pageTitle = 'Intelligent Minds';
 
+$view->count=0;
+$view->chatMsg = array("First Message","Second Message");
 
+if (isset($_POST['chatSnd'])) {
+    array_push($view->chatMsg,htmlentities($_POST['chatMsg']));
+}
 require_once('Views/index.phtml');
