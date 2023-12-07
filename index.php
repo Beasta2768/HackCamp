f9942051f9ecff2ee7ed7e59049b620d7db2c6bd
Beasta2768 <81296353+Beasta2768@users.noreply.github.com>
2023-12-07 18:01:03 +0000

    LLM API Intgeraton V1

--- index.php
+++ index.php
@@ -3,4 +3,16 @@
 $view = new stdClass();
 $view->pageTitle = 'Home Page';
 
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "http://127.0.0.1:5000/");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$curl_data = curl_exec($ch);
+curl_close($ch);
+
+$response = json_decode($curl_data);
+
+$view->aiResponse = $response->response;
+
 require_once('Views/index.phtml');
