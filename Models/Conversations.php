1a506f0209b889d4027b60899e50a16b181d90aa
Beasta2768 <81296353+Beasta2768@users.noreply.github.com>
2023-12-08 09:23:05 +0000

    Conversations % LLM Working

---
+++ Models/Conversations.php
@@ -0,0 +1,38 @@
+<?php
+
+class Conversations
+{
+    protected $key,$prompt,$response;
+
+    public function __construct($dbRow)
+    {
+        $this->key = $dbRow['key'];
+        $this->prompt = $dbRow['prompt'];
+        $this->response = $dbRow['response'];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrompt()
+    {
+        return $this->prompt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+
+}
