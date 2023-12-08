d0fb51ff3ef14e1b993ed8a9fe08ba4e421f168c
Adamsami7 <Adam.sami.12072004@gmail.com>
2023-12-08 16:24:14 +0000

    database connection in python

--- home.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify
-
-home_bp = Blueprint('home',__name__)
-@home_bp.route('/hello')
-def hello():
-    return jsonify({'aimodel':'openai',
-                    'airesponse':'Ai Test Response Here'})
-
