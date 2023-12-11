8ffb81d02d2345261808c0fe43bf3ff8617df95b
Beasta2768 <81296353+Beasta2768@users.noreply.github.com>
2023-12-11 15:59:24 +0000

    Working on file handling v1

--- LLM Api.py
+++ LLM Api.py
@@ -1,13 +1,19 @@
 from flask import jsonify,Flask,request,Blueprint
 from openAi import llmCall
+
 app = Flask(__name__)
 llm = llmCall()
 
+def test():
+    return 'test'
 
 @app.route('/')
 def airesponse():
     llmcall = llm.newprompt(request.args.get("prompt"))
     return jsonify({'response': llmcall})
 
+@app.route('/file')
+def fileParse():
+    llm.fileprocessor("TestLink")
 
 app.run(debug=True)
