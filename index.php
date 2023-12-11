7838978c3f9af7cafe7ffdfa2cbc46f9ee32805b
Alexander <vilhelmssonalex76@gmail.com>
2023-12-11 15:23:00 +0000

    Retouched up on smaller device optimisation with key focus on input forms

--- index.php
+++ index.php
@@ -1,31 +1,12 @@
 <?php
-require_once('Models/ConversationsDataSet.php');
-
 $view = new stdClass();
-$view->pageTitle = 'Home Page';
+$view->pageTitle = 'Intelligent Minds';
 
-$ch = curl_init();
+$view->count=0;
+$view->chatMsg = array();
 
-
-$conversations = new ConversationsDataSet();
-
-$view->conversations = $conversations->fetchAllConversations();
-$view->count = 0;
-
-if (isset($_POST['chatSnd'])) {
-    $prompt = str_replace(" ","%20",htmlentities($_POST['chatMsg']));
-
-    curl_setopt($ch, CURLOPT_URL, "http://127.0.0.1:5000/?prompt=".$prompt);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-    $curl_data = curl_exec($ch);
-    curl_close($ch);
-
-    $response = json_decode($curl_data);
-
-    $conversations->storeConversation(htmlentities($_POST['chatMsg']),$response->response);
-    $view->conversations = $conversations->fetchAllConversations();
-    echo $response->response;
+if (isset($_POST['Submit'])) {
+    array_push($view->chatMsg,htmlentities($_POST['chatMsg']));
+    echo $_POST['chatMsg'];
 }
-
 require_once('Views/index.phtml');
