4eadcd9cd6bf09b8c3e510f0584f71816ca18fb6
Beasta2768 <81296353+Beasta2768@users.noreply.github.com>
2023-12-11 15:59:24 +0000

    Working on file handling v1

--- openAi.py
+++ openAi.py
@@ -1,8 +1,10 @@
 import dotenv
-from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
-from langchain.schema import HumanMessage, SystemMessage
 from langchain.chains import ConversationChain
+from langchain.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
 
 dotenv.load_dotenv("llm.env")
 
@@ -14,3 +16,11 @@ class llmCall:
     def newprompt(self, prompt):
         nprompt = self.conversation.run(prompt)
         return nprompt
+
+    def fileprocessor(self,file):
+        loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
+        data = loader.load()
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+        all_splits = text_splitter.split_documents(data)
+        vectorstore = Chroma.from_documents(documents=all_splits, embedding=OpenAIEmbeddings())
+        print(vectorstore)
