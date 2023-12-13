4432bf7c295fbc0db0ad748f1618104a982bc73d
ALIHASANZAID <A.H.Zaidi153@gmail.com>
2023-12-13 15:51:40 +0000

    + Api Error Response

--- index.php
+++ index.php
@@ -13,12 +13,12 @@ $newChat = new differentChatsDataSet();
 
 $view->conversations = $conversations->fetchAllConversations();
 $view->chats = $newChat->fetchAllChats();
-$view->callError = false;
+$view->callError = null;
 
 if (isset($_POST['submit'])) {
     $prompt = str_replace(" ","%20",htmlentities($_POST['chatMsg']));
 
-    curl_setopt($ch, CURLOPT_URL, "http://realmheart.pythonanywhere.com/?prompt=".$prompt);
+    curl_setopt($ch, CURLOPT_URL, "http://127.0.0.1:5000/?prompt=".$prompt);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
     $curl_data = curl_exec($ch);
@@ -26,11 +26,11 @@ if (isset($_POST['submit'])) {
 
     $response = json_decode($curl_data);
 
-    if(isset($response)){
+    if(isset($response->response)){
     $conversations->storeConversation(htmlentities($_POST['chatMsg']),$response->response, 1);
     $view->conversations = $conversations->fetchAllConversations();
     } else{
-        $view->callError = true;
+        $view->callError = $response->error;
     }
 }
 
