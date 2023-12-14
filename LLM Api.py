a634c477f20834fb35a952075fd2baab0f76478e
Beasta2768 <81296353+Beasta2768@users.noreply.github.com>
2023-12-14 14:52:29 +0000

    File Handling V1

--- LLM Api.py
+++ LLM Api.py
@@ -14,10 +14,24 @@ llm = llmCall()
 @app.route('/')
 def airesponse():
     try:
-        # llmcall = llm.newprompt(request.args.get("prompt"))
-        return jsonify({'response': "llmcall", 'chat': request.args.get("chat")})
+        if (request.args.get("file")):
+            response = llm.fileprocessor(request.args.get("file"), request.args.get("prompt"))
+            return response
+        elif(request.args.get("prompt")):
+            llmcall = llm.newprompt(request.args.get("prompt"))
+            return jsonify({'response': llmcall, 'chat': request.args.get("chat")})
+        else:
+             return jsonify({'error': "No Prompt", 'message': "You Didn't Enter A prompt"})
     except Exception as ex:
         return jsonify({'error': type(ex).__name__, 'message': ex.args})
+
+@app.route('/file')
+def fileParse():
+    if(request.args.get("file")):
+            return jsonify({"file": request.args.get("file")})
+    else:
+        response = llm.fileprocessor("fileLink", "prompt")
+        return response
 
 
 # Run the App - Run the Flask application in debug mode if the script is executed directly.
--- openAi.py
+++ openAi.py
@@ -1,10 +1,12 @@
 import dotenv
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationChain
+from langchain.chains import ConversationChain, ConversationalRetrievalChain
 from langchain.document_loaders import WebBaseLoader
+from langchain.memory import ConversationSummaryMemory
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
+from langchain.load.dump import dumps
 
 dotenv.load_dotenv("llm.env")
 
@@ -17,10 +19,17 @@ class llmCall:
         nprompt = self.conversation.run(prompt)
         return nprompt
 
-    def fileprocessor(self,file):
-        loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
+    def fileprocessor(self,file,prompt):
+        loader = WebBaseLoader(file)
         data = loader.load()
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
         all_splits = text_splitter.split_documents(data)
         vectorstore = Chroma.from_documents(documents=all_splits, embedding=OpenAIEmbeddings())
-        print(vectorstore)
+
+        memory = ConversationSummaryMemory(
+            llm=self.chat, memory_key="chat_history", return_messages=True
+        )
+        retriever = vectorstore.as_retriever()
+        qa = ConversationalRetrievalChain.from_llm(self.chat, retriever=retriever, memory=memory)
+        response = qa(prompt)
+        return dumps(response.get("answer"))
